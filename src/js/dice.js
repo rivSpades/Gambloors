c1eec99ddab6e72d3aae8991b7d946d739565626
rivSpades <riverspades33@gmail.com>
2022-10-23 00:30:13 +0100

    Dice-v1.0

--- src/js/dice.js
+++ src/js/dice.js
@@ -1,42 +1,35 @@
 'use strict';
-window.onload=function(){
+window.onload = function () {
+  const calcWinChance = (rollUnder) =>
+    ((1 / (100 / rollUnder)) * 100).toFixed(0);
+  const calcPayout = (rollUnder, betsize, edge) => {
+    return (betsize * ((100 - edge) / rollUnder)).toFixed(0);
+  };
 
+  const edge = 2;
+  document.querySelector('.dice-output-payout-value').textContent =
+    calcPayout(50, 50, edge) + '$';
+  document.querySelector('.dice-output-winchance-value').textContent =
+    calcWinChance(50) + '%';
+  document.querySelector('.dice-roll-value').value =
+    document.querySelector('.dice-slider').value;
 
+  document.querySelector('.dice-slider').addEventListener('input', function () {
+    const rollUnder = document.querySelector('.dice-roll-value').value;
+    const betSize = document.querySelector('.dice-betsize-form-value').value;
 
+    document.querySelector('.dice-output-winchance-value').textContent =
+      calcWinChance(rollUnder) + '%';
+    document.querySelector('.dice-output-payout-value').textContent =
+      calcPayout(rollUnder, betSize, edge) + '$';
+  });
 
-const calcWinChance = rollUnder => ((1/(100/rollUnder))*100).toFixed(0);
-const calcPayout=(rollUnder,betsize,edge) =>{
-  return (betsize*((100-edge)/rollUnder)).toFixed(0);
-}
-
-
-
-const edge=2;
-document.querySelector('.output-payout-value').textContent= calcPayout(50,50,edge)+'$';
-document.querySelector('.output-win-chance-value').textContent = calcWinChance(50)+'%';
-document.querySelector('.roll-under-value').value=document.querySelector('.slider').value
-
-
-document.querySelector('.slider').addEventListener('input',function(){
-
-  const rollUnder= document.querySelector('.roll-under-value').value;
-  const betSize= document.querySelector('.bet-size-value').value;
-
-  document.querySelector('.output-win-chance-value').textContent = calcWinChance(rollUnder)+'%';
-  document.querySelector('.output-payout-value').textContent= calcPayout(rollUnder,betSize,edge)+'$';
-
-});
-
-
-document.querySelector('.bet-size-value').addEventListener('input',function(){
-const betSize= document.querySelector('.bet-size-value').value;
-const rollUnder= document.querySelector('.roll-under-value').value;
-document.querySelector('.output-payout-value').textContent= calcPayout(rollUnder,betSize,edge)+'$';
-
-});
-
-
-
-
-
-}
+  document
+    .querySelector('.dice-betsize-form-value')
+    .addEventListener('input', function () {
+      const betSize = document.querySelector('.dice-betsize-form-value').value;
+      const rollUnder = document.querySelector('.dice-roll-value').value;
+      document.querySelector('.dice-output-payout-value').textContent =
+        calcPayout(rollUnder, betSize, edge) + '$';
+    });
+};
