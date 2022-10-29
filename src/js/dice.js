42ac8933c6a63d300de732cf1dd3c5732975926b
rivSpades <riverspades33@gmail.com>
2022-10-29 17:32:57 +0100

    Dice-v1.3

--- src/js/dice.js
+++ src/js/dice.js
@@ -1,9 +1,25 @@
 'use strict';
 window.onload = function () {
-  const calcWinChance = (rollUnder) =>
-    ((1 / (100 / rollUnder)) * 100).toFixed(0);
-  const calcPayout = (rollUnder, betsize, edge) => {
-    return (betsize * ((100 - edge) / rollUnder)).toFixed(0);
+  const calcWinChance = (rollValue) => {
+    const currentRoll = document.querySelector(
+      '.dice-roll-container .dice-container-title'
+    ).textContent;
+    return (
+      (1 /
+        (100 / (currentRoll === 'Roll Under' ? rollValue : 100 - rollValue))) *
+      100
+    ).toFixed(0);
+  };
+  const calcPayout = (rollValue, betsize, edge) => {
+    const currentRoll = document.querySelector(
+      '.dice-roll-container .dice-container-title'
+    ).textContent;
+
+    return (
+      betsize *
+      ((100 - edge) /
+        (currentRoll === 'Roll Under' ? rollValue : 100 - rollValue))
+    ).toFixed(0);
   };
 
   const edge = 2;
@@ -47,7 +63,11 @@ window.onload = function () {
       //specify whats going to happen when click
 
       const value = randomInt(1, 100);
-      const rollUnder = document.querySelector('.dice-roll-value').value;
+      const rollValue = document.querySelector('.dice-roll-value').value;
+      const currentRoll = document.querySelector(
+        '.dice-roll-container .dice-container-title'
+      ).textContent;
+
       document.querySelector('.dice-slider-bubble').style.display = 'flex';
 
       getComputedStyle(document.querySelector('.dice-slider-bubble')).left ===
@@ -60,10 +80,37 @@ window.onload = function () {
             value + '%');
 
       document.querySelector('.dice-slider-bubble-value').textContent = value;
-      value < rollUnder
+      (value < rollValue && currentRoll === 'Roll Under') ||
+      (value > rollValue && currentRoll === 'Roll Over')
         ? (document.querySelector('.dice-slider-bubble').style.backgroundColor =
             'green')
         : (document.querySelector('.dice-slider-bubble').style.backgroundColor =
             'red');
+    });
+
+  document
+    .querySelector('.dice-btn-rollchange')
+    .addEventListener('click', function () {
+      const currentRoll = document.querySelector(
+        '.dice-roll-container .dice-container-title'
+      ).textContent;
+      currentRoll === 'Roll Under'
+        ? (document.querySelector(
+            '.dice-roll-container .dice-container-title'
+          ).textContent = 'Roll Over')
+        : (document.querySelector(
+            '.dice-roll-container .dice-container-title'
+          ).textContent = 'Roll Under');
+
+      //dice reset//
+      document.querySelector('.dice-slider-bubble').style.display = 'none';
+      document.querySelector('.dice-output-payout-value').textContent =
+        calcPayout(50, 50, edge) + '$';
+      document.querySelector('.dice-output-winchance-value').textContent =
+        calcWinChance(50) + '%';
+      document.querySelector('.dice-slider').value = 50;
+      document.querySelector('.dice-roll-value').value =
+        document.querySelector('.dice-slider').value;
+      document.querySelector('.dice-betsize-form-value').value = 50;
     });
 };
